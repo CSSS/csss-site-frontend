02e0db6cc83137ca247777c192a3d4f70bc81deb
Micah Baker <micahb923@gmail.com>
2024-02-21 13:40:00 -0800

    Update nginx deployment script

--- deploy_to_nginx.sh
+++ deploy_to_nginx.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo "Checking out the build branch..."
+cd /home/csss-site/csss-site-frontend
+if [ $? != 0 ]; then
+	echo "----"
+	echo "This script is meant only for CSSS servers."
+	exit 1
+fi
 
 # store current branch (e.g., "main") in current_branch
+echo "Checking out the build branch..."
 current_branch=$(git branch --show-current)
 
 git checkout build
@@ -14,70 +20,24 @@ fi
 
 # get latest updates then list the file tree
 git pull origin build
+
+echo "----"
 git ls-files
+echo "----"
 
-# default static files path for nginx
-deploy_path='/var/www/html'
+echo "Clearing /var/www/html..."
+rm -Rf /var/www/html/*
 
-# wait for user input before proceeding
-while true; do
-	echo "Proceed and deploy to $deploy_path? (This cannot be undone.)"
-	echo "(Y)es, (n)o, (o)ther path?"
-	read choice
-
-	if [ $choice = "Y" ]; then
-		break
-	elif [ $choice = "n" ]; then
-		exit 0
-	elif [ $choice = "o" ]; then
-		echo "What is the other path?"
-		read deploy_path
-	else
-		echo "Not sure what you mean..."
-	fi
-done
-
-echo "Removing build/ and node_modules/..."
-# we don't want to deploy the build and node_modules files
-rm -Rf build node_modules
-
-echo "Removing contents of $deploy_path..."
-sudo rm -Rf $deploy_path/*
-
-echo "Copying new files to $deploy_path..."
-sudo cp -Rf . $deploy_path
-sudo rm $deploy_path/.gitignore
-
-permissions='u=rwx,go=rx'
-user='www-data'
-group='www-data'
-
-# wait for user input before proceeding
-while true; do
-	echo "Permissions for $deploy_path will be set to $permissions for $user:$group."
-	echo "(C)ontinue, set (p)ermissions, (u)ser, (g)roup?"
-	read choice
-
-	if [ $choice = "C" ]; then
-		break
-	elif [ $choice = "p" ]; then
-		echo "What permissions?"
-		read permissions
-	elif [ $choice = "u" ]; then
-		echo "What user?"
-		read user
-	elif [ $choice = "g" ]; then
-		echo "What group?"
-		read group
-	else
-		echo "Not sure what you mean..."
-	fi
-done
-
-echo "Setting permissions and ownership..."
-sudo chown -R $user:$group $deploy_path
-sudo chmod -R $permissions $deploy_path
+# selectively copy build files to /var/www/html
+echo "Copying files to /var/www/html..."
+rsync \
+	--recursive \
+	--exclude="build/" \
+	--exclude="node_modules/" \
+	--exclude=".*" \
+	/home/csss-site/csss-site-frontend/* \
+	/var/www/html
 
 git checkout $current_branch
 
-echo "Finished! Check for yourself at $deploy_path."
+echo "Finished!"
