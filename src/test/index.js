29849c524daeb3baf6dd8899077c340c4ed4d807
Micah Baker <micahb923@gmail.com>
2024-02-22 20:46:31 -0800

    Configure webpack

--- src/test/index.js
+++ src/test/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <p>Hello, world!</p>
   </React.StrictMode>
 );
