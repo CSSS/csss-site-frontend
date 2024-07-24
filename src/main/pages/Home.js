bc6c2fed0c7d023923ba16ac08d058a02701f87f
dga48 <darrickgunawan@gmail.com>
2024-07-23 23:39:32 -0700

    fork

--- src/main/pages/Home.js
+++ src/main/pages/Home.js
@@ -4,22 +4,8 @@ import { Page } from '../components';
 export const Home = () => {
   return (
     <Page>
-      <div className="p-4">
-        <p>
-          WELCOME TO
-          <br />
-          THE <b>FUTURE WEBSITE</b>
-          <br />
-          OF THE <b>SIMON FRASER UNIVERSITY</b>
-          <br />
-          <b>COMPUTING SCIENCE STUDENT SOCIETY</b> (CSSS)!
-          <br />
-        </p>
-        <p className="text-slate-400">(Currently under construction!)</p>
-        <p className="text-blue-400">
-          <a href="https://sfucsss.org">Click here to go see the old site.</a>
-        </p>
-      </div>
+
+      
     </Page>
   );
 };
