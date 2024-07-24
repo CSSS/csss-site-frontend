2cce8f4e0da620f99c44194e5fae2b5f6c7314dc
dga48 <darrickgunawan@gmail.com>
2024-07-23 23:58:04 -0700

    added image

--- src/main/pages/Home.js
+++ src/main/pages/Home.js
@@ -4,8 +4,13 @@ import { Page } from '../components';
 export const Home = () => {
   return (
     <Page>
-
-      
+      <div className="relative h-screen w-full overflow-hidden">
+        <img
+          src="/homepage.jpg"
+          alt="SFU CSSS"
+          className="w-full h-full object-cover"
+        />
+      </div>
     </Page>
   );
 };
