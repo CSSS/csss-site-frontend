f817d3a2f5319580584d2368da8e159163bdab6a
Darrickg <darrickgunawan@gmail.com>
2024-07-24 01:23:03 -0700

    aaaaaaa

--- src/main/pages/Home.js
+++ src/main/pages/Home.js
@@ -4,12 +4,45 @@ import { Page } from '../components';
 export const Home = () => {
   return (
     <Page>
-      <div className="relative h-screen w-full overflow-hidden">
+      <div>
+        Computing Science Student Society abcdefghijglmnopqrstuvwxyzComputing
+        Science Student Society abcdefghijglmnopqrstuvwxyzComputing Science
+        Student Society abcdefghijglmnopqrstuvwxyzComputing Science Student
+        Society abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyzComputing Science Student Society
+        abcdefghijglmnopqrstuvwxyz
+      </div>
+      <div className="relative w-full h-screen">
         <img
           src="/homepage.jpg"
           alt="SFU CSSS"
-          className="w-full h-full object-cover"
+          className="absolute inset-0 w-full h-full object-cover"
         />
+        <div className="relative flex items-center justify-center h-full bg-black bg-opacity-50">
+          <h1 className="text-4xl sm:text-6xl md:text-8xl font-bold text-white leading-tight text-center">
+            Welcome to the
+            <br />
+            Simon Fraser University
+            <br />
+            Computing Science Student Society abcdefghijglmnopqrstuvwxyz
+          </h1>
+        </div>
       </div>
     </Page>
   );
