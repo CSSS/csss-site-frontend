62732cf6e1e0a98186206d4b024bf68bf875182e
Nakul Bansal <nakulbansal727@gmail.com>
2024-08-13 22:32:32 -0700

    Initialize Pages

---
+++ src/main/pages/Affiliates.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from '../components';
+
+export const Affiliates = () => {
+  return (
+    <Page>
+      <p>Affiliates</p>
+    </Page>
+  );
+};
---
+++ src/main/pages/Documents.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from '../components';
+
+export const Documents = () => {
+  return (
+    <Page>
+      <p>Documents</p>
+    </Page>
+  );
+};
