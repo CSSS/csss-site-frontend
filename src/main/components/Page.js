d116840bdec728c0438bba0193b6ecd8973961f0
Micah Baker <114637762+micahdbak@users.noreply.github.com>
2024-08-19 20:03:29 -0700

    Add Documents and Affiliates pages (#60)
    ---------
    
    Co-authored-by: Nakul Bansal <nakulbansal727@gmail.com>
    Co-authored-by: Nakul Bansal <120340476+Nakul727@users.noreply.github.com>

--- src/main/components/Page.js
+++ src/main/components/Page.js
@@ -33,6 +33,11 @@ export const Page = ({ children }) => {
         <VSCode.NavItem isInFolder={true} text="About.md" to="/about" />
         <VSCode.NavItem
           isInFolder={true}
+          text="Affiliates.md"
+          to="/affiliates"
+        />
+        <VSCode.NavItem
+          isInFolder={true}
           text="Common Rooms.md"
           to="/common_rooms"
         />
