ea9a7c306741faf984d88f0b85bdcdf435bf9e23
Micah Baker <114637762+micahdbak@users.noreply.github.com>
2024-08-19 20:03:29 -0700

    Add Documents and Affiliates pages (#60)
    ---------
    
    Co-authored-by: Nakul Bansal <nakulbansal727@gmail.com>
    Co-authored-by: Nakul Bansal <120340476+Nakul727@users.noreply.github.com>

--- src/main/components/index.js
+++ src/main/components/index.js
@@ -1,1 +1,2 @@
 export * from './Page.js';
+export * from './Footer.js';
--- src/main/index.js
+++ src/main/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-import { Home, Profile, NotFound } from './pages';
+import { Home, Profile, NotFound, Affiliates, Documents } from './pages';
 
 const router = createHashRouter([
   { path: '/', element: <Home /> },
   { path: '/profile', element: <Profile /> },
+  { path: '/affiliates', element: <Affiliates /> },
+  { path: '/documents', element: <Documents /> },
   { path: '*', element: <NotFound /> }
 ]);
 
