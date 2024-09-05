f3498ed0a4322f83392e860c72eaca6dc03a9b34
Micah Baker <micahb923@gmail.com>
2024-09-05 13:18:22 -0700

    Ran npm run lint

--- public/profile/script.js
+++ public/profile/script.js
@@ -1,12 +1,12 @@
 function toggleHidden(elementID, collapsibleID) {
-    let element = document.getElementById(elementID);
-    let collapsible = document.getElementById(collapsibleID);
+  let element = document.getElementById(elementID);
+  let collapsible = document.getElementById(collapsibleID);
 
-    if (element.getAttribute("hidden") !== null) {
-        element.removeAttribute("hidden");
-        collapsible.src = "/static/icons/minus.svg";
-    } else {
-        element.setAttribute("hidden", "");
-        collapsible.src = "/static/icons/chevron-down.svg";
-    }
+  if (element.getAttribute('hidden') !== null) {
+    element.removeAttribute('hidden');
+    collapsible.src = '/static/icons/minus.svg';
+  } else {
+    element.setAttribute('hidden', '');
+    collapsible.src = '/static/icons/chevron-down.svg';
+  }
 }
