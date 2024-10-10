9787af80015988e105c594604a7b638f5ae1717f
gunmack <jreedoy2002@yahoo.com>
2024-10-10 02:14:13 -0700

    refractored code for the page (split up into functions)

--- src/main/pages/CommonRooms.js
+++ src/main/pages/CommonRooms.js
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Page, Footer } from '../components';
 
 // TODO: Add new updated pic of ASB 9971
 
 const rooms = [
-  // {
-  //   text: 'ASB 9802',
-  //   image: '/static/files/main/asb9802.jpg',
-  //   link: 'https://roomfinder.sfu.ca/apps/sfuroomfinder_web/?q=ASB%209802',
-  //   svg: '/static/files/old-common-room.svg'
-  // },
   {
     text: 'ASB 9971',
     image: '/static/files/main/asb9971.png',
@@ -17,6 +11,8 @@ const rooms = [
     svg: '/static/files/new-common-room.svg'
   }
 ];
+
+const headers = ['ITEM', 'PRICE($)'];
 
 const pop = [
   { name: 'Coke', price: '1.25' },
@@ -38,22 +34,23 @@ const snacks = [
   { name: 'Protein shakes', price: '3.50' }
 ];
 
-export const CommonRooms = () => {
+{
+  /* descriptions */
+}
+const Description = () => {
   const listSpacing = 'px-4';
   return (
-    <Page style={{ fontFamily: 'Poppins, sans-serif' }}>
+    <div>
       <br></br>
       <br />
       <div className="flex justify-center text-3xl md:text-5xl font-bold">
-        Common Rooms
+        Common Room
       </div>
 
-      {/* descriptions  */}
       <div className="p-8 max-w-4xl mx-auto text-lg">
         <p className="text-left py-8">
-          ASB 9802 and ASB 9971, located in the Applied Sciences Building are
-          the CSSS common rooms maintained by the SFU CSSS. These rooms offer
-          several free services such as ⤵️
+          ASB 9971 located in the Applied Sciences Building is the CSSS's very
+          own common room. This room offers several services and amenities ⤵️
         </p>
 
         <table className="pl-8">
@@ -85,18 +82,26 @@ export const CommonRooms = () => {
           </tbody>
         </table>
       </div>
+    </div>
+  );
+};
 
-      {/* Photos of the common rooms */}
-      <div className="mx-16 my-8 flex flex-col md:flex-row gap-8">
+{
+  /* Photos of the common room */
+}
+const CRPhoto = () => {
+  return (
+    <div>
+      <div className="mx-16 md:px-16 my-16">
         {rooms.map((room) => {
           return (
             <div className="flex-1 lg:mx-16">
               <a href={room.link} target="_blank">
-                <div className="relative h-80 overflow-hidden rounded-xl transform transition-transform duration-300 hover:scale-105">
+                <div className="flex items-center justify-center relative h-96 w-full overflow-hidden rounded-xl transform transition-transform duration-300 hover:scale-105">
                   <img
                     src={room.image}
                     alt={room.text}
-                    className=" inset-0 w-full h-full object-cover"
+                    className="items-center w-full h-full object-cover"
                     style={{ 'overflow-clip-margin': 'unset' }}
                   />
                   <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent opacity-90"></div>
@@ -111,129 +116,133 @@ export const CommonRooms = () => {
           );
         })}
       </div>
+    </div>
+  );
+};
 
-      {/* svg maps of the rooms */}
-      <div className=" mx-16 flex flex-col md:flex-row gap-8">
+{
+  /* svg map of the room */
+}
+const Maps = () => {
+  return (
+    <div>
+      <div className=" mx-16 md:px-16 my-16 ">
         {rooms.map((room) => {
           return (
             <div
-              className="justify-center relative w-full h-80 rounded-xl
-             bg-white py-2 lg:mx-16"
+              className="flex-1 justify-center relative h-auto w-auto rounded-xl
+            bg-white"
             >
               <object height={300} width="100%" data={room.svg}></object>
             </div>
           );
         })}
       </div>
+    </div>
+  );
+};
 
-      {/*Refreshments*/}
+{
+  /*Refreshments*/
+}
+const Refreshments = () => {
+  return (
+    <div>
       <div className="p-16 max-w-4xl mx-auto text-left">
         <div className="flex text-4xl sm:text-3xl md:text-4xl font-bold">
           Refreshments
         </div>
         <br></br>
         <p className="text-xl">
-          ASB 9802 hosts our state of the art snack and pop vending machines
+          ASB 9971 hosts our state of the art snack and pop vending machines
           that are regularly stocked with sweet snacks and ice cold pop. The
           CSSS takes pride in selling the cheapest snacks and pop across all of
           Burnaby campus.
         </p>
       </div>
+    </div>
+  );
+};
 
-      {/*Table of prices*/}
+{
+  /*Creates tables*/
+}
+const Table = ({ data, headers }) => {
+  return (
+    <div className="-m-1.5 overflow-x-auto p-1.5 min-w-full">
+      <table className="divide-y border rounded">
+        <thead>
+          <tr className="bg-black text-white uppercase">
+            {headers.map((header, index) => (
+              <th key={index} className="px-6 py-3 text-left">
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr
+              key={index}
+              className={
+                index % 2 === 0
+                  ? 'bg-white text-gray-800'
+                  : 'bg-gray text-white'
+              }
+            >
+              <td className="px-6 py-4 text-left whitespace-nowrap">
+                {item.name}
+              </td>
+              <td className="px-6 py-4 text-right whitespace-nowrap">
+                {item.price}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
+{
+  /*Table of prices*/
+}
+const PriceTables = () => {
+  return (
+    <div>
       <div className="justify-center flex gap-2 max-w-4xl mx-auto overflow-hidden">
         <div className="mx-16 flex flex-col lg:flex-row gap-8 ">
           <div className="mx-8">
             <p className="text-center text-3xl font-bold">Snacks 🍫</p>
             <br></br>
-
-            <div class="-m-1.5 overflow-x-auto p-1.5 min-w-full inline-block align-middle overflow-hidden">
-              <table class="divide-y border rounded">
-                <thead>
-                  <tr className="bg-black  text-white uppercase" scope="col">
-                    <th class="px-6 py-3 text-left">ITEM</th>
-                    <th class="px-6 py-3 text-right">PRICE($)</th>
-                  </tr>
-                </thead>
-
-                {snacks.map((snack, index) => {
-                  const tdClass = 'px-6 py-4';
-                  return (
-                    <tbody
-                      key={index}
-                      className={
-                        index % 2 === 0
-                          ? 'bg-white text-gray-800'
-                          : 'bg-gray text-white'
-                      }
-                    >
-                      <tr className="whitespace-nowrap">
-                        <td class={tdClass} style={{ textAlign: 'left' }}>
-                          {snack.name}
-                        </td>
-                        <td class={tdClass} style={{ textAlign: 'right' }}>
-                          {snack.price}
-                        </td>
-                      </tr>
-                    </tbody>
-                  );
-                })}
-              </table>
-            </div>
+            <Table data={snacks} headers={headers} />
           </div>
-
-          <div className="flex flex-col mx-8">
+          <div className="mx-8">
             <p className="text-center text-3xl font-bold">Drinks 🥤</p>
             <br></br>
-            <div class="-m-1.5 overflow-x-auto p-1.5 min-w-full inline-block align-middle overflow-hidden">
-              <table class="min-w-full divide-y border rounded">
-                <thead>
-                  <tr className="bg-black">
-                    <th
-                      scope="col"
-                      class="px-6 py-3 text-left text-white uppercase"
-                    >
-                      ITEM
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 text-right text-white uppercase"
-                    >
-                      PRICE($)
-                    </th>
-                  </tr>
-                </thead>
-                {pop.map((pop, index2) => {
-                  const tdClass2 = 'px-6 py-4 whitespace-nowrap';
-                  return (
-                    <tbody
-                      key={index2}
-                      className={
-                        index2 % 2 === 0
-                          ? 'bg-white text-gray-800'
-                          : 'bg-gray text-white'
-                      }
-                    >
-                      <tr>
-                        <td class={tdClass2} style={{ textAlign: 'left' }}>
-                          {pop.name}
-                        </td>
-                        <td class={tdClass2} style={{ textAlign: 'right' }}>
-                          {pop.price}
-                        </td>
-                      </tr>
-                    </tbody>
-                  );
-                })}
-              </table>
-            </div>
+            <Table data={pop} headers={headers} />
           </div>
         </div>
       </div>
       <br></br>
+    </div>
+  );
+};
 
-      <Footer />
-    </Page>
+{
+  /*Main export function */
+}
+export const CommonRooms = () => {
+  return (
+    <div style={{ fontFamily: 'Poppins, sans-serif' }}>
+      <Page>
+        <Description />
+        <CRPhoto />
+        <Maps />
+        <Refreshments />
+        <PriceTables />
+        <Footer />
+      </Page>
+    </div>
   );
 };
