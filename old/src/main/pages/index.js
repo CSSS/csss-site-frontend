c0c2d0aeb8af45582e8f59e92817635c8ebc20da
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-15 01:42:33 -0700

    Started work on the Navbar

--- old/src/main/pages/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Home.js';
-export * from './NotFound.js';
-export * from './Profile.js';
-export * from './CommonRooms.js';
-export * from './Committees.js';
-export * from './Affiliates.js';
-export * from './Documents.js';
