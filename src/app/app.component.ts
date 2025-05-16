a7a0c406aab85379adce733f7f848a1dff285f3b
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-15 20:32:41 -0700

    Navbar entries now display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CsssCodeModule } from '../ui/csss-code/csss-code.module';
+import { navbarEntries } from 'pages/navbar-entries';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CsssCodeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'csss-ng-frontend';
+  navbarEntries = navbarEntries;
 }
