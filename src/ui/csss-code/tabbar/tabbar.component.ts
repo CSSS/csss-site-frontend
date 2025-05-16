2cac1ffafb5f973d554d96311f4e9ee8979b524a
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-16 00:40:30 -0700

    Added the tab bar. Added a hover effect on the nav bar items

---
+++ src/ui/csss-code/tabbar/tabbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'csss-tabbar',
+  standalone: false,
+  templateUrl: './tabbar.component.html',
+  styleUrl: './tabbar.component.scss'
+})
+export class TabbarComponent {}
