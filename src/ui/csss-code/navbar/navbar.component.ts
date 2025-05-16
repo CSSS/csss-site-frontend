366dbba413e5012a2a26469cb0b975817a4750b6
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-15 18:14:14 -0700

    Added navbar entries list

--- src/ui/csss-code/navbar/navbar.component.ts
+++ src/ui/csss-code/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'csss-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {}
