7ea1ab3a2f688db15ff3fe5987ed1f753a66b43b
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-15 21:56:23 -0700

    Refactored the navbar items to a different component.

--- src/ui/csss-code/csss-code.module.ts
+++ src/ui/csss-code/csss-code.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarItemComponent } from './navbar/navbar-item/navbar-item.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, NavbarItemComponent],
   imports: [CommonModule, FontAwesomeModule],
-  exports: [NavbarComponent]
+  exports: [NavbarComponent, NavbarItemComponent]
 })
 export class CsssCodeModule {}
--- src/ui/csss-code/navbar/navbar.component.ts
+++ src/ui/csss-code/navbar/navbar.component.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, OnInit, Signal } from '@angular/core';
 import { NavbarEntry } from 'pages/navbar-entries';
 import { faChevronDown, faChevronRight, faFile } from '@fortawesome/free-solid-svg-icons';
+
+interface NavbarItem extends NavbarEntry {
+  isClosed: boolean;
+}
 
 @Component({
   selector: 'csss-navbar',
@@ -9,10 +13,19 @@ import { faChevronDown, faChevronRight, faFile } from '@fortawesome/free-solid-s
   styleUrl: './navbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   entries = input.required<NavbarEntry[]>();
-
+  navEntries: NavbarItem[] = [];
   fileIcon = faFile;
   folderClosedIcon = faChevronRight;
   folderOpenIcon = faChevronDown;
+
+  ngOnInit(): void {
+    this.navEntries = this.entries().map(entry => {
+      return {
+        ...entry,
+        isClosed: true
+      };
+    });
+  }
 }
