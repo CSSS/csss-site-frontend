476715029f079b9a96c72bf1702b83dbbcd8189b
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-15 20:32:41 -0700

    Navbar entries now display

--- src/app/pages/navbar-entries.ts
+++ src/app/pages/navbar-entries.ts
@@ -9,72 +9,96 @@ export interface NavbarFolder {
   entries: NavbarEntry[];
 }
 
-export type NavbarEntry = NavbarFile | NavbarFolder;
+export const isNavbarFile = (entry: NavbarEntry) => {
+  return (entry as NavbarFolder).entries !== undefined;
+};
+
+export interface NavbarEntry {
+  key: string;
+  name: string;
+  type: 'file' | 'folder';
+  entries?: NavbarEntry[];
+}
 
 export const navbarEntries: NavbarEntry[] = [
   {
     key: 'readme',
-    name: 'README'
+    name: 'README',
+    type: 'file'
   },
   {
     key: 'home',
     name: 'The CSSS',
+    type: 'folder',
     entries: [
       {
         key: 'about',
-        name: 'About'
+        name: 'About',
+        type: 'file'
       },
       {
         key: 'affiliates',
-        name: 'Affiliates'
+        name: 'Affiliates',
+        type: 'file'
       },
       {
         key: 'committees',
-        name: 'Committees'
+        name: 'Committees',
+        type: 'file'
       },
       {
         key: 'common-room',
-        name: 'Common Room'
+        name: 'Common Room',
+        type: 'file'
       },
       {
         key: 'documents',
-        name: 'Documents'
+        name: 'Documents',
+        type: 'file'
       },
       {
         key: 'officers',
-        name: 'Officers'
+        name: 'Officers',
+        type: 'file'
       }
     ]
   },
   {
     key: 'elections',
     name: 'Elections',
+    type: 'folder',
     entries: [
       {
         key: 'elections-upcoming',
-        name: 'Upcoming'
+        name: 'Upcoming',
+        type: 'file'
       },
       {
         key: 'speeches',
-        name: 'Speeches'
+        name: 'Speeches',
+        type: 'file'
       },
       {
         key: 'elections-archive',
-        name: 'Archive'
+        name: 'Archive',
+        type: 'file'
       }
     ]
   },
   {
     key: 'events',
     name: 'Events',
+    type: 'folder',
     entries: [
       {
         key: 'events-upcoming',
-        name: 'Upcoming'
+        name: 'Upcoming',
+        type: 'file'
       },
       {
         key: 'events-archive',
-        name: 'Archive'
+        name: 'Archive',
+        type: 'file'
       }
     ]
   }
--- src/ui/csss-code/csss-code.module.ts
+++ src/ui/csss-code/csss-code.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule],
-  exports: [NavbarComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [NavbarComponent]
 })
 export class CsssCodeModule {}
