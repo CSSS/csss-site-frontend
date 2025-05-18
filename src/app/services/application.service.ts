0b47ccdd975f2c5213d69ded213584db85ae4f90
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-18 13:43:02 -0700

    Tabs now display running applications

--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
-import { updateSignalMap } from '../utils/signalUtils';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
-import { Application, applicationsMap } from './applications';
+import { Application, getApplicationById, routeApplicationMap } from './applications';
+import { updateSignalMap } from 'utils/signalUtils';
 
 /**
  * Interface representing an Application that is potentially running concurrently with other Applications.
@@ -34,15 +34,16 @@ export class ApplicationService {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
-        let splitRoute = event.urlAfterRedirects.split('/');
-        if (splitRoute.length > 0) {
-          const last = splitRoute.length - 1;
-          // Discard other params. This assumes that the URLs will have the form:
-          // `/<application>?=<query params>` or `/<runtime>/<application>?=<query params>`
-          // or maybe they don't have any query params at all.
-          splitRoute[last] = splitRoute[last].split('?')[0];
+        let route = event.urlAfterRedirects;
+        // Discard other params. This assumes that the URLs will have the form:
+        // `/<application>?=<query params>` or `/<runtime>/<application>?=<query params>`
+        // or maybe they don't have any query params at all.
+        route = route.split('?')[0];
+        console.log(route);
+        const app = routeApplicationMap.get(route);
+        if (app) {
+          this.openApplication(app);
         }
-        console.log(splitRoute);
       });
   }
 
@@ -56,16 +57,16 @@ export class ApplicationService {
     this.focusedApplication.set(app);
   }
 
-  openApplication(id: number): void {
+  openApplication(application: Application): void {
     // Check if the application is already running first.
-    const app = this.runningApplications().get(id);
+    const app = this.runningApplications().get(application.id);
     if (app) {
       this.focusOnApplication(app);
     } else {
       // Otherwise, launch it.
-      const newApp = applicationsMap.get(id);
+      const newApp = getApplicationById(application.id);
       if (!newApp) {
-        throw new Error(`No registered application identified by ${id}`);
+        throw new Error(`No registered application identified by ${application.id}`);
       }
       const appToLaunch: RunningApplication = { ...newApp, isFocus: true };
       this.focusedApplication.set(appToLaunch);
