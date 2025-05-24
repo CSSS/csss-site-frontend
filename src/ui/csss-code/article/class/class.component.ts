e4610fe834e3d86f3b2df87359b3c916958a39a0
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-23 21:46:24 -0700

    Adding more components to improve styling of the static information pages

---
+++ src/ui/csss-code/article/class/class.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'code-class',
+  standalone: false,
+  templateUrl: './class.component.html',
+  styleUrl: './class.component.scss'
+})
+export class ClassComponent {}
