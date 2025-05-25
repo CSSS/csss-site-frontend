f48663f8a3525acf0de69d828a2a05a43420376e
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-25 13:21:55 -0700

    Base structure set up
    * About and History sections added

--- public/tech_fair/2025/v2/main.js
+++ public/tech_fair/2025/v2/main.js
@@ -1,9 +1,7 @@
-let overlayEl, overlayText;
+let overlayEl, overlayText, progressBar;
 
-function setOverlay() {
-  const scrollTop = window.scrollY;
-  const maxScroll = window.innerHeight;
-  const percent = Math.min(scrollTop / maxScroll, 1);
+function setOverlay(distanceScrolled, windowHeight) {
+  const percent = Math.min(distanceScrolled / windowHeight, 1);
   const fade = percent * 50;
 
   overlayEl.style.background = `linear-gradient(to right,
@@ -16,15 +14,27 @@ function setOverlay() {
 
   overlayText.style.opacity = 1 - percent;
 }
+
+function setProgressBar(distanceScrolled, windowHeight) {
+  const docHeight = document.documentElement.scrollHeight - windowHeight;
+  const percent = (distanceScrolled / docHeight) * 100;
+  console.log(percent);
+  progressBar.style.width = `${percent}%`;
+}
+
 function onload() {
   overlayEl = document.querySelector('.overlay');
   overlayText = document.querySelector('.overlay-text');
-  setOverlay();
+  progressBar = document.querySelector('.progress-bar');
+  document.body.classList.remove('loading');
+  setOverlay(window.scrollY, window.innerHeight);
+  setProgressBar(window.scrollY, window.innerHeight);
 
   /**
    * Event listener for the overlay to open up.
    */
   window.addEventListener('scroll', () => {
-    setOverlay();
+    setOverlay(window.scrollY, window.innerHeight);
+    setProgressBar(window.scrollY, window.innerHeight);
   });
 }
