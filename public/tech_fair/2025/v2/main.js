d95f7174d124ea4d76f6ca72c14dbd4e3170c480
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-29 19:33:20 -0700

    Basic shape of the website laid out.

--- public/tech_fair/2025/v2/main.js
+++ public/tech_fair/2025/v2/main.js
@@ -1,12 +1,32 @@
-const observer = new IntersectionObserver(entries => {
-  for (const entry of entries) {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
+function setPopInEffect() {
+  const observer = new IntersectionObserver(entries => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
     }
-  }
-});
+  });
 
-const sections = document.querySelectorAll('.hidden');
-for (const section of sections) {
-  observer.observe(section);
+  const sections = document.querySelectorAll('.hidden');
+  for (const section of sections) {
+    observer.observe(section);
+  }
 }
+
+function setHeaderScroll() {
+  const header = document.getElementById('header');
+  let lastScrollY = 0;
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScrollY) {
+      header.style.top = '-5rem';
+    } else if (parseFloat(header.style.top) < 5) {
+      header.style.top = '0';
+    }
+    lastScrollY = window.scrollY;
+  });
+}
+
+(() => {
+  setHeaderScroll();
+  setPopInEffect();
+})();
