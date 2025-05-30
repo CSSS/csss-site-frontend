ffb2499e5c352b75bfd7b92e735da4de3166474a
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-05-29 22:22:48 -0700

    Finished the students section.

--- public/tech_fair/2025/v2/main.js
+++ public/tech_fair/2025/v2/main.js
@@ -15,11 +15,31 @@ function setPopInEffect() {
 
 function setHeaderScroll() {
   const header = document.getElementById('header');
+  const navs = document.querySelectorAll('a[href^="#"]');
+  let ignoreScroll = false;
   let lastScrollY = 0;
+
+  // Disables the header hiding when scrolling down if a navigation link is pressed.
+  for (const nav of navs) {
+    nav.addEventListener('click', () => {
+      ignoreScroll = true;
+
+      setTimeout(() => {
+        lastScrollY = window.scrollY;
+        ignoreScroll = false;
+      }, 1000);
+    });
+  }
+
+  // Sets the header to hide/show when scrolling down/up on the page.
   window.addEventListener('scroll', () => {
+    if (ignoreScroll) {
+      return;
+    }
+
     if (window.scrollY > lastScrollY) {
       header.style.top = '-5rem';
-    } else if (parseFloat(header.style.top) < 5) {
+    } else {
       header.style.top = '0';
     }
     lastScrollY = window.scrollY;
