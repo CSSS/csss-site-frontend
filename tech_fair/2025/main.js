7bb55527ce9ff7fdfbb2bed97cecf6d05f62d004
jbriones1 <jbriones1@user.noreply.github.com>
2025-06-06 00:21:40 +0000

    Updating  with PR#112: Update to Tech Fair

---
+++ tech_fair/2025/main.js
@@ -0,0 +1,59 @@
+/** Variables declared in the :root component in `style.css` */
+const cssVars = getComputedStyle(document.querySelector(':root'));
+
+/** The size of rem, in pixels */
+const remSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+/** The size of the header in pixels. */
+const headerSize = parseFloat(cssVars.getPropertyValue('--header-height')) * remSize;
+
+/** Checks if the page is scrolled to the top. */
+let isAtTop = window.scrollY > headerSize;
+
+function setPopInEffect() {
+  const observer = new IntersectionObserver(entries => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    }
+  });
+
+  const sections = document.querySelectorAll('.hidden');
+  for (const section of sections) {
+    observer.observe(section);
+  }
+}
+
+function checkScrolledEffect(scrolledElements) {
+  if (window.scrollY <= headerSize && !isAtTop) {
+    isAtTop = true;
+    for (const el of scrolledElements) {
+      el.classList.remove('scrolled');
+    }
+  } else if (window.scrollY > headerSize && isAtTop) {
+    isAtTop = false;
+    for (const el of scrolledElements) {
+      el.classList.add('scrolled');
+    }
+  }
+}
+
+function setScrolledEffect() {
+  const scrolledElements = [
+    document.getElementById('header'),
+    document.getElementById('nav-list'),
+    document.getElementById('home-logo')
+  ];
+
+  checkScrolledEffect(scrolledElements);
+
+  window.addEventListener('scroll', () => {
+    checkScrolledEffect(scrolledElements);
+  });
+}
+
+(() => {
+  setPopInEffect();
+  setScrolledEffect();
+})();
