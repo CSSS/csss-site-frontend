b7aede30e6513767cefa35e52fdccf6394cb8887
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-10 00:30:34 -0700

    feat: Added a CSSS Button component
    * Added some buttons on the home page to better direct people

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { csssLogo } from '../../../assets/icons/csss-logo';
+import { csssLogo } from 'assets/icons/csss-logo';
+import { CsssCodeModule } from '@csss-code/csss-code.module';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'csss-home',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CsssCodeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   csssIcon = csssLogo;
+  fileIcon = faFile;
+
+  private router = inject(Router);
+
+  aboutAction() {
+    this.router.navigate(['/about']);
+  }
+
+  readmeAction() {
+    this.router.navigate(['/readme']);
+  }
 }
