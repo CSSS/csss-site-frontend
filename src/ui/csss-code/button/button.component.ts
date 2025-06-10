2ed4af59542df1e5e9dd83a437be8a069a814da7
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-10 00:30:34 -0700

    feat: Added a CSSS Button component
    * Added some buttons on the home page to better direct people

---
+++ src/ui/csss-code/button/button.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'csss-button',
+  standalone: false,
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ButtonComponent {
+  /**
+   * The text on the button.
+   */
+  label = input.required<string>();
+
+  /**
+   * The icon of the button.
+   */
+  icon = input<IconDefinition>();
+}
