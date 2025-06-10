62445c04cea7afa8c34ab7b48634034887338a79
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-10 09:39:47 -0700

    chore: Removed unused tab item component

--- src/ui/csss-code/csss-code.module.ts
+++ src/ui/csss-code/csss-code.module.ts
@@ -5,7 +5,6 @@ import { TabBarComponent } from './tabbar/tabbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarItemComponent } from './navbar/navbar-item/navbar-item.component';
 import { ArticleComponent } from './article/article.component';
-import { TabItemComponent } from './tabbar/tab-item/tab-item.component';
 import { ClassComponent } from './article/class/class.component';
 import { FunctionComponent } from './article/function/function.component';
 import { ButtonComponent } from './button/button.component';
@@ -16,7 +15,6 @@ import { ButtonComponent } from './button/button.component';
     NavbarItemComponent,
     TabBarComponent,
     ArticleComponent,
-    TabItemComponent,
     ClassComponent,
     FunctionComponent,
     ButtonComponent
