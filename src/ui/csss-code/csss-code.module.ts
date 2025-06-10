610fa3cc79fd97c61c9385e103c27d96452bc08a
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-10 10:36:54 -0700

    feat: Removed class and function decorator components

--- src/ui/csss-code/csss-code.module.ts
+++ src/ui/csss-code/csss-code.module.ts
@@ -5,8 +5,6 @@ import { TabBarComponent } from './tabbar/tabbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarItemComponent } from './navbar/navbar-item/navbar-item.component';
 import { ArticleComponent } from './article/article.component';
-import { ClassComponent } from './article/class/class.component';
-import { FunctionComponent } from './article/function/function.component';
 import { ButtonComponent } from './button/button.component';
 import { RouterModule } from '@angular/router';
 
@@ -16,8 +14,6 @@ import { RouterModule } from '@angular/router';
     NavbarItemComponent,
     TabBarComponent,
     ArticleComponent,
-    ClassComponent,
-    FunctionComponent,
     ButtonComponent
   ],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
@@ -26,7 +22,6 @@ import { RouterModule } from '@angular/router';
     NavbarItemComponent,
     TabBarComponent,
     ArticleComponent,
-    ClassComponent,
     ButtonComponent
   ]
 })
