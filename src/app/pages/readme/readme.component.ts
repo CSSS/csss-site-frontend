0ff604e8e32cbeeb8fae65f4d6ee45919f2182ae
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-11 00:44:25 -0700

    feat: Added the common room card button to ReadMe page

--- src/app/pages/readme/readme.component.ts
+++ src/app/pages/readme/readme.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CsssCodeModule } from '@csss-code/csss-code.module';
 import { FooterComponent } from 'components/footer/footer.component';
 
 @Component({
   selector: 'csss-readme',
-  imports: [CsssCodeModule, FooterComponent],
+  imports: [CsssCodeModule, FooterComponent, RouterModule],
   templateUrl: './readme.component.html',
   styleUrl: './readme.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
