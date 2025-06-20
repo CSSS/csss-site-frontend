d776b79f3f59cf7ed4ff4e6e1e5e56c7aff620a8
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-19 19:57:35 -0700

    feat: Fixed the header and added more scrolling

--- public/frosh/2025/main.js
+++ public/frosh/2025/main.js
@@ -1,3 +1,8 @@
+const styleVars = window.getComputedStyle(document.body);
+
+/**
+ * Animates the reeds parting when scrolling down.
+ */
 function reedsParting() {
   const scrollTrigger = {
     trigger: '.hero',
@@ -18,6 +23,9 @@ function reedsParting() {
   });
 }
 
+/**
+ * Makes the photos pop in from below.
+ */
 function imagePopIn() {
   document.querySelectorAll('.gallery').forEach(gallery => {
     const photos = gallery.querySelectorAll('.photo');
@@ -25,8 +33,7 @@ function imagePopIn() {
     gsap.from(photos, {
       scrollTrigger: {
         trigger: gallery,
-        start: 'top 80%',
-        toggleActions: 'play none none none'
+        start: 'top 80%'
       },
       y: '5vh',
       opacity: 0,
@@ -37,9 +44,40 @@ function imagePopIn() {
   });
 }
 
+function headerGradient() {
+  gsap.to('header', {
+    scrollTrigger: {
+      trigger: 'main',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true
+    },
+    backgroundColor: styleVars.getPropertyValue('--c-bot-main')
+  });
+
+  const navTrigger = {
+    trigger: 'main',
+    start: 'center center',
+    toggleActions: 'play none play reverse',
+    markers: true
+  };
+
+  gsap.to('header', {
+    scrollTrigger: navTrigger,
+    color: 'white'
+  });
+
+  // For the svg of the logo
+  gsap.to('.st1', {
+    scrollTrigger: navTrigger,
+    fill: 'white'
+  });
+}
+
 window.addEventListener('load', _ => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
   reedsParting();
+  headerGradient();
   if (window.matchMedia('(min-width: 1280px').matches) {
     imagePopIn();
   }
