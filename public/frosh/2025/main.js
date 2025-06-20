bdf202705fa0f9dfe9acdd7ad9a7ca31803fccd3
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-19 17:44:56 -0700

    feat: Added in image pop in

--- public/frosh/2025/main.js
+++ public/frosh/2025/main.js
@@ -18,7 +18,29 @@ function reedsParting() {
   });
 }
 
+function imagePopIn() {
+  document.querySelectorAll('.gallery').forEach(gallery => {
+    const photos = gallery.querySelectorAll('.photo');
+
+    gsap.from(photos, {
+      scrollTrigger: {
+        trigger: gallery,
+        start: 'top 80%',
+        toggleActions: 'play none none none'
+      },
+      y: '5vh',
+      opacity: 0,
+      duration: 1.2,
+      ease: 'power3.out',
+      stagger: 0.2
+    });
+  });
+}
+
 window.addEventListener('load', _ => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
   reedsParting();
+  if (window.matchMedia('(min-width: 1280px').matches) {
+    imagePopIn();
+  }
 });
