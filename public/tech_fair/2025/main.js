7c80c191c0ecc4287ea15ccee7e1e5ce77317c19
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-06-28 00:26:09 -0700

    feat: Reduced the width and reworded some sections.

--- public/tech_fair/2025/main.js
+++ public/tech_fair/2025/main.js
@@ -19,13 +19,13 @@ function setPopInEffect() {
       photo,
       {
         opacity: 0,
-        x: -100,
+        y: 100,
         duration: 2
       },
       {
         scrollTrigger: photo,
         opacity: 1,
-        x: 0
+        y: 0
       }
     );
   }
