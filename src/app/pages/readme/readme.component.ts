6f238373d1f9e14178ef5023ad7378cbffca0751
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-06 13:31:15 -0700

    feat: Fixed some styling for the README page

--- src/app/pages/readme/readme.component.ts
+++ src/app/pages/readme/readme.component.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CsssCodeModule } from '@csss-code/csss-code.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'csss-readme',
-  imports: [CsssCodeModule, RouterModule],
+  imports: [CsssCodeModule, RouterModule, FontAwesomeModule],
   templateUrl: './readme.component.html',
   styleUrl: './readme.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ReadMeComponent {}
+export class ReadMeComponent {
+  arrowIcon = faArrowDown;
+}
