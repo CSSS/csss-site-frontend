1ce70c0003d7d351ea031ec61247629d5f435221
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-06 12:38:09 -0700

    feat: Removed the footer for now

--- src/app/pages/readme/readme.component.ts
+++ src/app/pages/readme/readme.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CsssCodeModule } from '@csss-code/csss-code.module';
-import { FooterComponent } from 'components/footer/footer.component';
 
 @Component({
   selector: 'csss-readme',
-  imports: [CsssCodeModule, FooterComponent, RouterModule],
+  imports: [CsssCodeModule, RouterModule],
   templateUrl: './readme.component.html',
   styleUrl: './readme.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
