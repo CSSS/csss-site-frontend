5867ce5b3dc6020cef94e37cd185bf94e6fc2b11
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-06 17:25:13 -0700

    chore: Removed the About section
    * Will just move its contents to other pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { HomeComponent } from 'pages/home/home.component';
 
 export const routes: Routes = [
   {
-    path: 'about',
-    loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
-  },
-  {
     path: 'affiliates',
     loadComponent: () =>
       import('./pages/affiliates/affiliates.component').then(m => m.AffiliatesComponent)
