8df7944f81cc1e624435e1b1f764836ef0479722
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-08 14:22:11 -0700

    feat: Breakpoint strings should automatically build

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,6 +1,20 @@
-import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  HostBinding,
+  inject,
+  OnDestroy,
+  signal,
+  Signal,
+  WritableSignal
+} from '@angular/core';
 import { CsssCodeModule } from '@csss-code/csss-code.module';
+import { Subscription } from 'rxjs';
 import { ApplicationService } from 'services/application.service';
+import { BREAKPOINT_STRING_MAP } from 'styles/breakpoints';
 
 export interface TabBarItem {
   label: string;
@@ -14,11 +28,18 @@ export interface TabBarItem {
   styleUrl: './tabs.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TabsComponent {
+export class TabsComponent implements AfterViewInit, OnDestroy {
+  @HostBinding('style.height')
+  get height() {
+    return this.isHidden() ? '0px' : '2rem';
+  }
+
   /**
    * Gives the tabs the applications to display.
    */
   private applicationService = inject(ApplicationService);
+
+  private breakpointObs = inject(BreakpointObserver);
 
   /**
    * The tabs that should be displayed.
@@ -31,13 +52,39 @@ export class TabsComponent {
     return result;
   });
 
-  /**
-   *
-   */
+  private isSmall: WritableSignal<boolean> = signal(false);
+
   isHidden: Signal<boolean> = computed(
-    () => this.applicationService.runningApplications().size === 0
+    () => this.isSmall() && this.applicationService.runningApplications().size === 0
   );
 
+  private breakpointSub?: Subscription;
+
+  /**
+   * Hide the tab bar if the screen size is small and there are no applications running.
+   */
+
+  ngAfterViewInit(): void {
+    this.breakpointSub = this.breakpointObs
+      .observe(BREAKPOINT_STRING_MAP['small'])
+      .subscribe(res => {
+        if (res.breakpoints[BREAKPOINT_STRING_MAP['small']]) {
+          this.isSmall.set(false);
+        } else {
+          this.isSmall.set(true);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.breakpointSub?.unsubscribe();
+  }
+
+  /**
+   * Informs the application service to close a tab.
+   *
+   * @param index - The index of the tab to close.
+   */
   closeTab(index: number): void {
     this.applicationService.closeApplication(index);
   }
--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,3 +1,13 @@
 export const VIEW_BREAKPOINTS = {
   small: 968
 } as const;
+
+export const BREAKPOINT_STRING_MAP: Record<string, string> = Object.entries(
+  VIEW_BREAKPOINTS
+).reduce(
+  (acc, [k, v]) => {
+    acc[k] = `(min-width: ${v}px)`;
+    return acc;
+  },
+  {} as Record<string, string>
+);
