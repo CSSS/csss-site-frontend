9e839f7ddb304a8c1ba385e0236590811087acd1
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-08 08:03:10 -0700

    feat: Added the Common Room page

---
+++ src/app/pages/common-room/common-room.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommonRoomComponent } from './common-room.component';
+
+describe('CommonRoomComponent', () => {
+  let component: CommonRoomComponent;
+  let fixture: ComponentFixture<CommonRoomComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CommonRoomComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CommonRoomComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
