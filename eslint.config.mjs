24424f0b049fdba2693ac732c620b00dd62f9de1
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-09 12:01:58 -0700

    feat: Integrated eslint and prettier together

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier
+);
