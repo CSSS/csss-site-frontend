ca2a73fbb4f0469ec716ee170d7e3645ef435ec1
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-08 22:10:49 -0700

    feat: Added a dynamic timeline bar

--- src/app/pages/officers/officers.component.ts
+++ src/app/pages/officers/officers.component.ts
@@ -1,6 +1,16 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  ElementRef,
+  signal,
+  viewChild
+} from '@angular/core';
 import { CsssCodeModule } from '@csss-code/csss-code.module';
 import { ExecutiveAdministration, executives } from './officers';
+
+const REM = 16;
 
 @Component({
   selector: 'csss-officers',
@@ -9,6 +19,35 @@ import { ExecutiveAdministration, executives } from './officers';
   styleUrl: './officers.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OfficersComponent {
-  displayExecs = signal<ExecutiveAdministration>(executives[0]);
+export class OfficersComponent implements AfterViewInit {
+  protected displayExecs = signal<ExecutiveAdministration>(executives[0]);
+
+  protected yearsToDisplay = computed(() => {
+    const result = [];
+    const currentYear = Math.trunc(new Date().getFullYear());
+    for (let i = 0; i < this.lineWidth() - 1; i++) {
+      result.push(currentYear - i);
+    }
+    return result;
+  });
+
+  private timeline = viewChild.required<ElementRef<HTMLDivElement>>('timeline');
+
+  private lineWidth = signal<number>(0);
+
+  private resizeObs!: ResizeObserver;
+
+  ngAfterViewInit(): void {
+    this.resizeObs = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        console.log(entry.contentRect);
+        const linesToDisplay = Math.trunc(entry.contentRect.width / (6 * REM));
+
+        if (this.lineWidth() !== linesToDisplay) {
+          this.lineWidth.set(linesToDisplay);
+        }
+      }
+    });
+    this.resizeObs.observe(this.timeline().nativeElement);
+  }
 }
