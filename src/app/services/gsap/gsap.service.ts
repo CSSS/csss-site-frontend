44e10e34478d031c4836ef3b1d88415ec010c072
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-10 11:24:29 -0700

    feat: Flipping cards works

---
+++ src/app/services/gsap/gsap.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { gsap } from 'gsap';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GsapService {
+  /**
+   * GSAP's Match Media singleton.
+   * Add animations to this that will run based on current media query conditions.
+   */
+  private _mm?: gsap.MatchMedia;
+
+  get media(): gsap.MatchMedia {
+    if (!this._mm) {
+      this._mm = gsap.matchMedia();
+    }
+    return this._mm;
+  }
+}
