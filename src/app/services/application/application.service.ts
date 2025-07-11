fb0fff4a70adb0a4927036aecb26123e0ad735ef
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-10 22:57:08 -0700

    feat: Tab changes
    * Tabs can now open their saved view
    * Added a hover glow to the tab button
    * Reworked how applications are stored and fetched

--- src/app/services/application/application.service.ts
+++ src/app/services/application/application.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 import { addToSignalMap, removeFromSignalMap } from 'utils/signalUtils';
-import { Application, routeApplicationMap } from './applications';
+import { AppInfo, getApplicationByRoute } from './applications';
 
 /**
  * Service that handles updating what applications are currently running.
@@ -11,12 +11,20 @@ import { Application, routeApplicationMap } from './applications';
   providedIn: 'root'
 })
 export class ApplicationService {
-  focusedApplication: WritableSignal<Application | null> = signal<Application | null>(null);
-  runningApplications: WritableSignal<Map<number, Application>> = signal<Map<number, Application>>(
+  /**
+   * Application currently on the router
+   */
+  focusedApplication: WritableSignal<AppInfo | null> = signal<AppInfo | null>(null);
+
+  /**
+   * Map of applications that are running.
+   * Key is the application ID, value is the application info.
+   */
+  runningApplications: WritableSignal<Map<number, AppInfo>> = signal<Map<number, AppInfo>>(
     new Map()
   );
 
-  private router = inject(Router);
+  router = inject(Router);
 
   constructor() {
     // Observable that emits when navigating to a new URL has completed.
@@ -28,8 +36,8 @@ export class ApplicationService {
         // Discard other params. This assumes that the URLs will have the form:
         // `/<application>?=<query params>` or `/<runtime>/<application>?=<query params>`
         // or maybe they don't have any query params at all.
-        route = route.split('?')[0];
-        const app = routeApplicationMap.get(route);
+        route = route.split('?')[0]; // Just the route params
+        const app = getApplicationByRoute(route);
         if (app) {
           this.openApplication(app);
         }
@@ -39,25 +47,30 @@ export class ApplicationService {
   /**
    * Focus on an application, making its outlet the visible one.
    *
-   * @param app - Target application
+   * @param id - Target application ID
    */
-  // private focusOnApplication(app: Application): void {
-  //   if (this.focusedApplication()?.id !== app.id) {
-  //     this.focusedApplication.set(app);
-  //   }
-  // }
+  private focusOnApplication(id: number): void {
+    if (this.focusedApplication()?.id !== id) {
+      const app = this.runningApplications().get(id);
+      if (!app) {
+        throw new Error(`No running app with id ${id}`);
+      }
+      this.focusedApplication.set(app);
+    }
+  }
 
   /**
    * Try to open the application.
    *
    * @param application - Application to try and launch.
    */
-  private openApplication(application: Application): void {
+  private openApplication(application: AppInfo): void {
     const focusedApp = this.focusedApplication();
 
     if (focusedApp?.id === application.id) {
       return;
     }
+
     this.focusedApplication.set(application);
     addToSignalMap(this.runningApplications, application.id, application);
 
