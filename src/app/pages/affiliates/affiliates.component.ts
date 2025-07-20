735db46f8d1c88b398c0efe5fa970a50351c0fb2
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-20 11:19:33 -0700

    wip: move affiliates to its own file

--- src/app/pages/affiliates/affiliates.component.ts
+++ src/app/pages/affiliates/affiliates.component.ts
@@ -1,58 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CsssCodeModule } from '@csss-code/csss-code.module';
 import { prependHttps, toLocalImageUrl } from 'utils/stringUtils';
-
-interface Affiliate {
-  label: string;
-  imgName: string; // assumes the image is in the `public/affiliates` folder and is a `.png`
-  url: string; // URL to their resources
-}
-
-/**
- * Alphabetical order of our affiliates
- */
-const AFFILIATES: Affiliate[] = [
-  {
-    label: 'DSSS',
-    imgName: 'dsss',
-    url: 'discord.com/invite/kdj7F82mgn'
-  },
-  {
-    label: 'ESSS',
-    imgName: 'esss',
-    url: 'www.sfu.ca/esss/?page=home'
-  },
-  {
-    label: 'MSESS',
-    imgName: 'msess',
-    url: 'msess.ca'
-  },
-  {
-    label: 'MSU',
-    imgName: 'msu',
-    url: 'sfumsu.github.io'
-  },
-  {
-    label: 'SEESS',
-    imgName: 'seess',
-    url: 'seess.ca'
-  },
-  {
-    label: 'SSSS',
-    imgName: 'ssss',
-    url: 'sfussss.org'
-  },
-  {
-    label: 'WiCS',
-    imgName: 'wics',
-    url: 'www.sfu.ca/computing/wics'
-  },
-  {
-    label: 'WiE',
-    imgName: 'wie',
-    url: 'www.sfu.ca/clubs/wie.html'
-  }
-];
+import { AFFILIATES } from './affiliates';
 
 @Component({
   selector: 'csss-affiliates',
