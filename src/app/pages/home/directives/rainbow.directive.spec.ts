8214a08b88324255c5c58d7e374b5be28c33a707
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-23 09:44:42 -0700

    wip: removed all source files

--- src/app/pages/home/directives/rainbow.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RainbowDirective } from './rainbow.directive';
-
-describe('RainbowDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RainbowDirective();
-    expect(directive).toBeTruthy();
-  });
-});
