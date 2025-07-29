10dda7231c508bdccd87248d303d7667903a2519
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-07-29 16:33:27 -0700

    wip: add comments and change style of tabs

--- src/ui/csss-code/tabs/tab-group/tab-group.component.ts
+++ src/ui/csss-code/tabs/tab-group/tab-group.component.ts
@@ -7,6 +7,7 @@ import {
   contentChildren,
   effect,
   ElementRef,
+  HostListener,
   signal,
   viewChildren
 } from '@angular/core';
@@ -20,28 +21,58 @@ import { CodeTabComponent } from '../tab.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CodeTabGroupComponent implements AfterViewInit, AfterContentInit {
+  @HostListener('window:resize')
+  onResize(): void {
+    const tab = this.activeTab();
+    if (!tab) {
+      return;
+    }
+
+    this.setSelectorStyle();
+  }
+
+  /**
+   * The CodeTabComponents supplied into the CodeTabGroupComponent.
+   */
   protected tabComponents = contentChildren(CodeTabComponent);
+
+  /**
+   * The tab buttons that allow you to choose which tab to open.
+   */
   protected tabLabelEls = viewChildren<ElementRef<HTMLDivElement>>('tab');
+
+  /**
+   * The tab that has its content currently displayed.
+   */
   protected activeTab = signal<CodeTabComponent | null>(null);
+
+  /**
+   * A record that is used to update the CSS styles of the selector.
+   */
   protected selectorStyles = signal<Record<string, string>>({});
 
   constructor() {
+    // Handles the active tab changing.
     effect(() => {
       const activeTab = this.activeTab();
       if (!activeTab) {
         return;
       }
       for (const tab of this.tabComponents()) {
-        if (activeTab !== tab) {
-          tab.isActive.set(false);
+        if (activeTab === tab) {
+          tab.isActive.set(true);
+          const selectedTabEl = this.tabLabelEls()[tab.tabIndex()];
+          this.selectorStyles.set({
+            left: `${selectedTabEl.nativeElement.offsetLeft}px`,
+            width: `${selectedTabEl.nativeElement.offsetWidth}px`
+          });
+          this.setSelectorStyle();
           continue;
         }
-        tab.isActive.set(true);
-        const selectedTabEl = this.tabLabelEls()[tab.tabIndex()];
-        this.selectorStyles.set({
-          left: `${selectedTabEl.nativeElement.offsetLeft}px`,
-          width: `${selectedTabEl.nativeElement.offsetWidth}px`
-        });
+
+        if (tab.isActive()) {
+          tab.isActive.set(false);
+        }
       }
     });
   }
@@ -64,5 +95,22 @@ export class CodeTabGroupComponent implements AfterViewInit, AfterContentInit {
     }
 
     this.activeTab.set(this.tabComponents()[0]);
+  }
+
+  /**
+   * Changes the style of the tab selector, which makes it move to the currently active tab.
+   *
+   */
+  private setSelectorStyle(): void {
+    const activeTab = this.activeTab();
+    if (!activeTab) {
+      return;
+    }
+
+    const selectedTabEl = this.tabLabelEls()[activeTab.tabIndex()];
+    this.selectorStyles.set({
+      left: `${selectedTabEl.nativeElement.offsetLeft}px`,
+      width: `${selectedTabEl.nativeElement.offsetWidth}px`
+    });
   }
 }
--- src/ui/csss-code/tabs/tab.component.ts
+++ src/ui/csss-code/tabs/tab.component.ts
@@ -11,7 +11,8 @@ import {
   selector: 'code-tab',
   host: {
     '[attr.aria-selected]': 'isActive()',
-    '[attr.aria-disabled]': 'isDisabled()'
+    '[attr.aria-disabled]': 'isDisabled()',
+    '[attr.aria-label]': 'label()'
   },
   imports: [],
   templateUrl: './tab.component.html',
@@ -19,9 +20,28 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CodeTabComponent {
+  /**
+   * The index of the tab, based on the current order from left to right.
+   */
   tabIndex = signal<number>(-1);
+
+  /**
+   * Text on the tab button.
+   */
   label = input.required<string>();
+
+  /**
+   * True if it's the active tab, false otherwise.
+   */
   isActive = signal<boolean>(false);
+
+  /**
+   * True if the tab is disabled, false otherwise.
+   */
   isDisabled = signal<boolean>(false);
+
+  /**
+   * The element reference of the tab.
+   */
   elementRef = inject(ElementRef);
 }
