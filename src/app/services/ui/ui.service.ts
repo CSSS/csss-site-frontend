a48312784072c9a5f5c48dd482ca46cb43cc5ca6
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-08-09 01:38:36 -0700

    feat: add UI service

---
+++ src/app/services/ui/ui.service.ts
@@ -0,0 +1,29 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { inject, Injectable, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { BREAKPOINT_STRING_MAP } from 'styles/breakpoints';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiService {
+  /**
+   * Flag representing if the menu is closed or open.
+   */
+  isFileSystemOpen = signal<boolean>(false);
+
+  /**
+   * Observer to watch when a breakpoint is hit.
+   */
+  breakpointObs = inject(BreakpointObserver);
+
+  isLargeViewport = toSignal(
+    this.breakpointObs.observe(BREAKPOINT_STRING_MAP['large']).pipe(
+      map(bp => {
+        return bp.breakpoints[BREAKPOINT_STRING_MAP['large']];
+      })
+    ),
+    { initialValue: false }
+  );
+}
