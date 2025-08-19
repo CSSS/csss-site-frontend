6a0c4623093eef0cac884036d7a8453455a319cb
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-08-19 09:56:55 -0700

    wip: add route and navbar entry

---
+++ src/app/pages/event-archives/event-archives.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'cs-event-archives',
+  imports: [],
+  templateUrl: './event-archives.component.html',
+  styleUrl: './event-archives.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EventArchivesComponent {
+
+}
