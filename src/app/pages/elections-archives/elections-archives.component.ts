174870a4540141ab2bcfecd7c7ec3c0986fff19c
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-08-26 18:28:23 -0700

    wip: add year selector to Elections Archive

--- src/app/pages/elections-archives/elections-archives.component.ts
+++ src/app/pages/elections-archives/elections-archives.component.ts
@@ -1,10 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ArticleComponent } from '@csss-code/article/article.component';
+import { ElectionsService } from 'libs/backend-api';
 
 @Component({
   selector: 'cs-elections-archive',
-  imports: [],
+  imports: [ArticleComponent],
   templateUrl: './elections-archives.component.html',
   styleUrl: './elections-archives.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ElectionsArchivesComponent {}
+export class ElectionsArchivesComponent {
+  private electionsService = inject(ElectionsService);
+  protected elections = toSignal(this.electionsService.getElectionsList(), { initialValue: [] });
+  protected years = computed<Set<number>>(() => {
+    return this.elections().reduce(
+      (set, e) => set.add(new Date(e.datetime_start_nominations).getFullYear()),
+      new Set<number>()
+    );
+  });
+}
