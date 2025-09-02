37b4dbbb8108a1e678f9b2b96c3c11ef87c4028f
Melody <melody.oh1412@gmail.com>
2025-09-02 15:22:14 -0700

    added affiliate clubs and compressed imgs

--- src/app/pages/affiliates/affiliates.ts
+++ src/app/pages/affiliates/affiliates.ts
@@ -15,9 +15,18 @@ export interface Affiliate {
 export const AFFILIATES: Affiliate[] = [
   // Student Unions
   {
+    label: 'SSSS',
+    imgName: 'ssss',
+    websiteUrl: 'sfussss.org',
+    discordUrl: 'discord.gg/hY7WjXt',
+    fullName: 'Software Systems Student Society',
+    type: 'union'
+  },
+  {
     label: 'DSSS',
     imgName: 'dsss',
-    discordUrl: 'https://discord.gg/rcUvvYBUSF',
+    websiteUrl: 'www.facebook.com/sfudsss',
+    discordUrl: 'discord.gg/rcUvvYBUSF',
     fullName: 'Data Science Student Society',
     type: 'union'
   },
@@ -33,37 +42,31 @@ export const AFFILIATES: Affiliate[] = [
     label: 'MSESS',
     imgName: 'msess',
     websiteUrl: 'msess.ca',
-    discordUrl: 'https://discord.gg/zpXtC57',
+    discordUrl: 'discord.gg/zpXtC57',
     fullName: 'Mechatronic Systems Engineering Student Society',
-    type: 'union'
-  },
-  {
-    label: 'MSU',
-    imgName: 'msu',
-    websiteUrl: 'sfumsu.github.io',
-    fullName: 'Mathematics Student Union',
     type: 'union'
   },
   {
     label: 'SEESS',
     imgName: 'seess',
     websiteUrl: 'seess.ca',
+    discordUrl: 'discord.gg/w9vpfvHhVY',
     fullName: 'Sustainable Energy Engineering Student Society',
     type: 'union'
   },
   {
-    label: 'SSSS',
-    imgName: 'ssss',
-    websiteUrl: 'sfussss.org',
-    discordUrl: 'https://discord.gg/hY7WjXt',
-    fullName: 'Software Systems Student Society',
+    label: 'MSU',
+    imgName: 'msu',
+    websiteUrl: 'sfumsu.github.io',
+    discordUrl: 'discord.gg/nYXZYNTE4G',
+    fullName: 'Mathematics Student Union',
     type: 'union'
   },
   {
     label: 'SUS',
     imgName: 'sus',
     websiteUrl: 'sfususwebmaster.wordpress.com',
-    discordUrl: 'https://discord.gg/pKPdMjPZTb',
+    discordUrl: 'discord.gg/pKPdMjPZTb',
     fullName: 'Science Undergraduate Society',
     type: 'union'
   },
@@ -71,6 +74,7 @@ export const AFFILIATES: Affiliate[] = [
     label: 'WiCS',
     imgName: 'wics',
     websiteUrl: 'www.sfu.ca/computing/wics',
+    discordUrl: 'discord.gg/gEct3gUKbu',
     fullName: 'Women in Computing Science',
     type: 'union'
   },
@@ -78,16 +82,65 @@ export const AFFILIATES: Affiliate[] = [
     label: 'WiE',
     imgName: 'wie',
     websiteUrl: 'www.sfu.ca/clubs/wie.html',
+    discordUrl: 'discord.com/invite/UKf4WupZBC',
     fullName: 'Women in Engineering',
+    type: 'union'
+  },
+  {
+    label: 'CSGSA',
+    imgName: 'csga',
+    websiteUrl: 'www.sfu.ca/csgsa',
+    discordUrl: 'discord.gg/DEkf7Bp',
+    fullName: 'Computing Science Graduate Student Association',
+    type: 'union'
+  },
+  {
+    label: 'CS Alumni Group',
+    imgName: 'alumni',
+    websiteUrl: 'www.sfu.ca/alumni',
+    discordUrl: 'discord.gg/fZ8xVw3PtN',
+    fullName: 'Computing Science Alumni Group',
     type: 'union'
   },
   // Clubs
   {
+    label: 'Surge',
+    imgName: 'surge',
+    websiteUrl: 'sfusurge.com/',
+    discordUrl: 'discord.com/invite/xu7CqKc',
+    fullName: 'Surge',
+    type: 'club'
+  },
+  {
     label: 'Competitive Programming',
     imgName: 'competitive-programming',
-    websiteUrl: 'https://go.sfss.ca/clubs/857/info',
-    discordUrl: 'https://discord.gg/2mFZD2F',
+    websiteUrl: 'go.sfss.ca/clubs/857/info',
+    discordUrl: 'discord.gg/2mFZD2F',
     fullName: 'Competitive Programming',
+    type: 'club'
+  },
+  {
+    label: 'Game Dev Club',
+    imgName: 'game-dev-club',
+    websiteUrl: 'sfugamedev.club',
+    discordUrl: 'discord.gg/7JJDDyX',
+    fullName: 'Game Developers Club',
+    type: 'club'
+  },
+  {
+    label: 'Open Source Dev Club',
+    imgName: 'osdev',
+    websiteUrl: 'sfuosdev.com',
+    discordUrl: 'discord.com/invite/8CSRTyPXJ4',
+    fullName: 'Open Source Dev Club',
+    type: 'club'
+  },
+  {
+    label: 'DNS',
+    imgName: 'dns',
+    websiteUrl: 'sfudns.co',
+    discordUrl: 'discord.gg/cWV8PktVXR',
+    fullName: 'Developers & Systems Club',
     type: 'club'
   }
 ];
