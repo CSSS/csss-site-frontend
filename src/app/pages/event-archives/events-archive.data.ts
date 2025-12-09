4197c02eb67e7e07787e42ef27f9ceed77db5661
Jon Andre Briones <56126200+jbriones1@users.noreply.github.com>
2025-12-09 15:53:34 -0800

    Fix: Incorrect Mountain Madness year (#217)
    * Fixed the Mountain Madness year from 2024/2025 -> 2026
    * Updated the Events Archive for all completed events

--- src/app/pages/event-archives/events-archive.data.ts
+++ src/app/pages/event-archives/events-archive.data.ts
@@ -1,9 +1,11 @@
+// Years should be inserted from newest -> oldest
+// Add to these once the event is over
 export const FROSH_YEARS = [
-  2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012
+  2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012
 ] as const;
 
-export const FALL_HACKS_YEARS = [2024, 2020] as const;
+export const FALL_HACKS_YEARS = [2025, 2024, 2020] as const;
 
-export const TECH_FAIR_YEARS = [2024, 2023, 2022] as const;
+export const TECH_FAIR_YEARS = [2025, 2024, 2023, 2022] as const;
 
 export const MOUNTAIN_MADNESS_YEARS = [2024, 2023, 2022, 2021, 2020, 2019] as const;
