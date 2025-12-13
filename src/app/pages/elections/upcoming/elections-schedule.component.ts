d04be193f7a65dada5d8a1c089ed5bb662e1ff89
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-12-12 21:03:03 -0800

    fix: updated the styling of code-button
    * hover colour is now green
    * padding reduced from 2rem to 1rem
    * removed borders
    * no longer rounded

--- src/app/pages/elections/upcoming/elections-schedule.component.ts
+++ src/app/pages/elections/upcoming/elections-schedule.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ArticleComponent } from '@csss-code/article/article.component';
+import { ButtonComponent } from '@csss-code/button/button.component';
 
 @Component({
   selector: 'cs-upcoming',
-  imports: [ArticleComponent],
+  imports: [ArticleComponent, ButtonComponent],
   templateUrl: './elections-schedule.component.html',
   styleUrl: './elections-schedule.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
