fb4d578dded7da3c7d1ccb38bcfe2e2ca3db018c
Connor Jones <cja100@sfu.ca>
2026-07-04 22:08:30 -0700

    angular-calendar mvp
    *MVP: Emphasis on the m...
    
    * Installed angular-calendar after reading the docs page
    * This thing has loads of boilerplate... but it works!
    * I essentially just copied what I saw in the docs page, but there are also some live demos that we can take inspiration from.
    * I removed the ability for scarf to view our install analytics (?). The docs page mentions it being optional so I removed it. Note the change in package.json
    * The docs also say in the FAQ that this calendar isn't optimized for mobile. This might need some future tweaking to look good on mobile. From a 10s test in the browser dev tools, I can confidently say its not terrible
    
    TODO:
      * Style it up nice
      * Connect to the get all events endpoint in the backend (can we even do that rn?)
      * Alternatively, could hardcode some recurring events such as meetings

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -4,10 +4,18 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { ExternalLinkComponent } from 'components/url/external-link/external-link.component';
 import { EXTERNAL_LINKS } from 'components/url/links.data';
+import { DateAdapter, provideCalendar, CalendarPreviousViewDirective, CalendarTodayDirective, CalendarNextViewDirective, CalendarMonthViewComponent, CalendarWeekViewComponent, CalendarDayViewComponent, CalendarEvent, CalendarView, CalendarDatePipe } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @Component({
   selector: 'cs-events',
-  imports: [CodeArticleComponent, FontAwesomeModule, ExternalLinkComponent],
+  imports: [CodeArticleComponent, FontAwesomeModule, ExternalLinkComponent, CalendarPreviousViewDirective, CalendarTodayDirective, CalendarNextViewDirective, CalendarMonthViewComponent, CalendarWeekViewComponent, CalendarDayViewComponent, CalendarDatePipe],
+  providers: [
+    provideCalendar({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -15,4 +23,25 @@ import { EXTERNAL_LINKS } from 'components/url/links.data';
 export class EventsComponent {
   externalLinkIcon = faUpRightFromSquare;
   roomFinderLink = EXTERNAL_LINKS['boardgameRoomMap'];
+
+  readonly CalendarView = CalendarView;
+  view: CalendarView = CalendarView.Month;
+
+  // By 'Open' they mean expanded.
+  activeDayIsOpen = false;
+  closeOpenMonthViewDay(): void {
+    this.activeDayIsOpen = false;
+  }
+
+  viewDate = new Date();
+  events: CalendarEvent[] = [
+    {
+      start: new Date(),
+      title: 'CSSS Events',
+    },
+  ];
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
 }
