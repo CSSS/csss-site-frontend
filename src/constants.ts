30afa17d8f8cebdb1cfc558a70d0739e18245e89
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-07-26 15:23:19 -0700

    feat: finish committees page
    * added prettier auto-formatting on imports
    * added GradientCard component

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const ACCENT_COLOUR_ORDER = ['red', 'blue', 'pink', 'teal'] as const;
+
+export type AccentColour = (typeof ACCENT_COLOUR_ORDER)[number];
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client" />
+
+declare module '*.astro' {
+  import type { AstroComponentFactory } from 'astro/runtime/server/index.js';
+
+  const component: AstroComponentFactory;
+  export default component;
+}
