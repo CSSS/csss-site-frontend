586b80b4509bf90f7f2eb28958d57999e2f67d72
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-07-28 02:40:52 -0700

    chore: clean up public folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
+import compress from 'astro-compress';
+
 export default defineConfig({
-  site: 'https://sfucsss.org',
+  site: 'https://new.sfucsss.org',
   output: 'static',
-  integrations: [mdx()]
+  integrations: [mdx(), compress()]
 });
+
