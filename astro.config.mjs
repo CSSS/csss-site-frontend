91b6aa225ff8c87f7217e82caad469fdc3f4d79c
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-09-03 00:44:47 -0700

    refactor: change how fonts are defined and loaded

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import compress from 'astro-compress';
 
@@ -8,6 +8,64 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://new.sfucsss.org',
   output: 'static',
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Cal Sans',
+      cssVariable: '--font-heading',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/cal-sans-v2-latin-regular.woff2'],
+            weight: 'normal',
+            style: 'normal'
+          }
+        ]
+      }
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'Geist Mono',
+      cssVariable: '--font-body',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/geist-mono-v6-latin-regular.woff2'],
+            weight: 'normal',
+            style: 'normal'
+          }
+        ]
+      }
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'Geist Mono',
+      cssVariable: '--font-geist-mono',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/geist-mono-v6-latin-700.woff2'],
+            weight: 700,
+            style: 'bold'
+          }
+        ]
+      }
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'Geist Mono',
+      cssVariable: '--font-geist-mono',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/geist-mono-v6-latin-italic.woff2'],
+            weight: 'normal',
+            style: 'italic'
+          }
+        ]
+      }
+    }
+  ],
   integrations: [
     mdx(),
     sitemap(),
