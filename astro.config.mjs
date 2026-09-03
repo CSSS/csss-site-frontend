f74a877e381456d3ea8c0ae42c0b4887a8a99037
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-09-03 00:07:58 -0700

    feat: add sitemap generator and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import compress from 'astro-compress';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://new.sfucsss.org',
   output: 'static',
   integrations: [
     mdx(),
+    sitemap(),
     compress({
       CSS: false,
       HTML: {
